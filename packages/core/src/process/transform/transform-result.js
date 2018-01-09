791c373edb01eb364ced230d94b256f62286412f
Richard Miles <richard.miles@fast-chat.co.uk>
2018-01-09 23:07:04 +0000

    Delete some .js files that had snuck in

--- packages/core/src/process/transform/transform-result.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TransformResult = /** @class */ (function () {
-    function TransformResult() {
-    }
-    return TransformResult;
-}());
-exports.TransformResult = TransformResult;
