8f83c0e20f665e19a2251eda592d307be45e2b10
Richard Miles <richard.miles@fast-chat.co.uk>
2018-01-12 19:44:38 +0000

    Move TransformResult into the Transform file

--- packages/core/src/process/transform/transform-result.ts
+++
@@ -1,7 +0,0 @@
-import {GCodeFile} from "../../gcode/gcode-file";
-
-export class TransformResult {
-
-  readonly result: GCodeFile;
-  readonly status?: any;
-}
--- packages/core/src/process/transform/transform.ts
+++ packages/core/src/process/transform/transform.ts
@@ -1,7 +1,12 @@
-import {TransformResult} from './transform-result';
 import {GCodeFile} from "../../gcode/gcode-file";
 
 export interface Transform {
 
   transform(incoming: GCodeFile): TransformResult;
+}
+
+export class TransformResult {
+
+    readonly result: GCodeFile;
+    readonly status?: any;
 }
