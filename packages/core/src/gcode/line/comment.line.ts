4c84f0106f34136f40298b711e268cf389bdb181
Richard Miles <richard.miles@fast-chat.co.uk>
2018-01-12 20:53:34 +0000

    Remove output() from CommentLine

--- packages/core/src/gcode/line/comment.line.ts
+++ packages/core/src/gcode/line/comment.line.ts
@@ -1,12 +1,7 @@
 import {Line, LineType} from './line';
 
 export class CommentLine extends Line {
-
   constructor(comment: string) {
     super(LineType.COMMENT, comment);
-  }
-
-  output(): string {
-    return '';
   }
 }
