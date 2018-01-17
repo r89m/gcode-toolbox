a492629e4c64c1b895238439110dfb6ad0f8043b
Richard Miles <richard.miles@fast-chat.co.uk>
2018-01-17 22:44:55 +0000

    Initial implementation of CycleTimeAnalysis

---
+++ packages/process/src/analysis/cycle-time/cycle-time.analysis.spec.ts
@@ -0,0 +1,127 @@
+import {CycleTimeAnalysis, CycleTimeAnalysisResults} from "./cycle-time.analysis";
+import {Plane, PlaneSelectionCommand} from "../../../../gcode/src/command/plane-selection.command";
+import {MovementMode, SetMovementModeCommand} from "../../../../gcode/src/command/set-movement-mode.command";
+import {SpindleDirection, SpindleOnCommand} from "../../../../gcode/src/command/spindle.command";
+import {MoveFeed, MoveRapid} from "../../../../gcode/src/command/move-linear.command";
+
+describe("Cycle Time Analysis", () => {
+
+    const analyse = (input): CycleTimeAnalysisResults => {
+        return new CycleTimeAnalysis().analyse(input);
+    };
+
+    it("should report nothing for an empty file", () => {
+
+        expect(analyse([])).toEqual({
+            totalDuration: 0,
+            feedDuration: 0,
+            rapidDuration: 0
+        });
+    });
+
+    it("should report nothing for a file with no moves", () => {
+
+        const input = [
+            new PlaneSelectionCommand(Plane.XY),
+            new SetMovementModeCommand(MovementMode.ABSOLUTE),
+            new SpindleOnCommand(SpindleDirection.Clockwise)
+        ];
+
+        expect(analyse(input)).toEqual({
+            totalDuration: 0,
+            feedDuration: 0,
+            rapidDuration: 0
+        });
+    });
+
+    it("should report nothing for a file with only one move", () => {
+
+        const input = [
+            new MoveFeed(30, 10, 50)
+        ];
+
+        expect(analyse(input)).toEqual({
+            totalDuration: 0,
+            feedDuration: 0,
+            rapidDuration: 0
+        });
+    });
+
+    it("should report the correct duration for a file with only one feed move", () => {
+
+        const input = [
+            new MoveFeed(30, 10, 50, 100),
+            new MoveFeed(130)
+        ];
+
+        expect(analyse(input)).toEqual({
+            totalDuration: 60,
+            feedDuration: 60,
+            rapidDuration: 0
+        });
+    });
+
+    it("should report the correct duration for a file with only feed moves", () => {
+
+        const input = [
+            new MoveFeed(30, 10, 50, 100),
+            new MoveFeed(130),
+            new MoveFeed(undefined, 50, 100, 300)
+        ];
+
+        expect(analyse(input)).toEqual({
+            totalDuration: 70,
+            feedDuration: 70,
+            rapidDuration: 0
+        });
+    });
+
+    it("should report the correct duration for a file with only one rapid move", () => {
+
+        const input = [
+            new MoveRapid(30, 10, 50, 100),
+            new MoveRapid(130)
+        ];
+
+        expect(analyse(input)).toEqual({
+            totalDuration: 60,
+            feedDuration: 0,
+            rapidDuration: 60
+        });
+    });
+
+    it("should report the correct duration for a file with only rapid moves", () => {
+
+        const input = [
+            new MoveRapid(30, 10, 50, 100),
+            new MoveRapid(130),
+            new MoveRapid(undefined, 50, 100, 300)
+        ];
+
+        expect(analyse(input)).toEqual({
+            totalDuration: 70,
+            feedDuration: 0,
+            rapidDuration: 70
+        });
+    });
+
+    fit("should report the correct duration for a file with feed and rapid moves", () => {
+
+        const input = [
+            new MoveRapid(30, 10, 50, 100),
+            new MoveFeed(undefined, undefined, 0, 50),
+            new MoveFeed(130, undefined, undefined, 1000),
+            new MoveFeed(undefined, 210),
+            new MoveFeed(30),
+            new MoveFeed(undefined, 10),
+            new MoveRapid(undefined, undefined, 50),
+            new MoveRapid(undefined, 50, 100)
+        ];
+
+        expect(analyse(input)).toEqual({
+            totalDuration: 102,
+            feedDuration: 96,
+            rapidDuration: 6
+        });
+    });
+});
--- packages/process/src/analysis/cycle-time/cycle-time.analysis.ts
+++ packages/process/src/analysis/cycle-time/cycle-time.analysis.ts
@@ -1,19 +1,96 @@
 import {Analysis} from '../analysis';
 import {Command} from "../../../../gcode/src/command/command";
+import {MoveFeed, MoveRapid} from "../../../../gcode/src/command/move-linear.command";
+
+const DEFAULT_SETTINGS:MachineSpeedSettings = {
+    maxFeedRateX: Infinity,
+    maxFeedRateY: Infinity,
+    maxFeedRateZ: Infinity,
+};
 
 export class CycleTimeAnalysis implements Analysis {
 
+  constructor(private readonly machineSpeedSettings:MachineSpeedSettings = DEFAULT_SETTINGS){}
+
   analyse(incoming: Command[]): CycleTimeAnalysisResults {
-    return {
-      rapidDuration: 300,
-      feedDuration: 400,
-      totalDuration: 700
-    };
+
+      const durations: CycleTimeAnalysisResults = {
+          totalDuration: 0,
+          feedDuration: 0,
+          rapidDuration: 0
+      };
+      let position: Position = undefined;
+      let currentFeedRate = 1;
+
+      for (let command of incoming) {
+          if (command instanceof MoveFeed || command instanceof MoveRapid) {
+
+              currentFeedRate = (command.feedRate / 60) || currentFeedRate;
+
+              // If this is the first move, record the starting position
+              if (position === undefined) {
+                  position = {
+                      x: command.x || 0,
+                      y: command.y || 0,
+                      z: command.z || 0
+                  };
+              } else {
+                  const newPosition = {
+                      x: command.x !== undefined ? command.x : position.x,
+                      y: command.y !== undefined ? command.y : position.y,
+                      z: command.z !== undefined ? command.z : position.z,
+                  };
+
+                  const delta = {
+                      x: newPosition.x - position.x,
+                      y: newPosition.y - position.y,
+                      z: newPosition.z - position.z,
+                  };
+
+                  const duration = {
+                      x: Math.abs(delta.x / (Math.min(currentFeedRate, this.machineSpeedSettings.maxFeedRateX))),
+                      y: Math.abs(delta.y / (Math.min(currentFeedRate, this.machineSpeedSettings.maxFeedRateY))),
+                      z: Math.abs(delta.z / (Math.min(currentFeedRate, this.machineSpeedSettings.maxFeedRateZ)))
+                  };
+
+                  const maxDuration = Math.max(duration.x, duration.y, duration.z);
+
+                  durations.totalDuration += maxDuration;
+                  if (command instanceof MoveFeed) {
+                      durations.feedDuration += maxDuration;
+                  } else {
+                      durations.rapidDuration += maxDuration;
+                  }
+
+                  position = newPosition;
+              }
+          }
+      }
+
+    return durations;
   }
+}
+
+interface Position {
+    x?: number,
+    y?: number,
+    z?: number
 }
 
 export interface CycleTimeAnalysisResults {
   rapidDuration?: number;
   feedDuration?: number;
   totalDuration?: number;
+}
+
+export interface MachineSpeedSettings {
+    maxFeedRateX?:number;
+    maxFeedRateY?:number;
+    maxFeedRateZ?:number;
+
+    // TODO: Not yet supported
+    rapidFeedRate?: number;
+    accelerationX?:number;
+    accelerationY?:number;
+    accelerationZ?:number;
 }
