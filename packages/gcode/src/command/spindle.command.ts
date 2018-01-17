d7c1a0b747c303e1eddb2adc8dd617b5d78d8b98
Richard Miles <richard.miles@fast-chat.co.uk>
2018-01-17 22:44:41 +0000

    Make Spindle RPM optional

--- packages/gcode/src/command/spindle.command.ts
+++ packages/gcode/src/command/spindle.command.ts
@@ -1,7 +1,7 @@
 import {Command, CommandType} from "./command";
 
 export class SpindleOnCommand extends Command {
-    constructor(public readonly direction:SpindleDirection, public readonly rpm:number, comment?:string) {
+    constructor(public readonly direction:SpindleDirection, public readonly rpm?:number, comment?:string) {
         super(CommandType.SPINDLE_ON, comment)
     }
 }
